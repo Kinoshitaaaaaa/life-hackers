1facaadbe02ae48041489ef59a60f452526edd65
Kinoshitaaaaaa <st0342e14@gmail.com>
2023-08-26 11:10:06 +0000

    seed update

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,6 +30,12 @@ genres = Genre.create!(
 olivia = User.create!(email: 'olivia@example.com', user_name: 'Lucas', password: 'password', introduction: 'よろしくお願いします！！')
 olivia.profile_image.attach(io: File.open("#{Rails.root}/db/fixtures/sample-user1.jpg"), filename:"sample-user1.jpg")
 
+james = User.create!(email: 'James@example.com', user_name: 'James', password: 'password', introduction: 'パンケーキにハマってます。')
+james.profile_image.attach(io: File.open("#{Rails.root}/db/fixtures/sample-user2.jpg"), filename:"sample-user2.jpg")
+
+lucas = User.create!(email: 'Lucas@example.com', user_name: 'Lucas', password: 'password', introduction: '釣り仲間募集してます。')
+lucas.profile_image.attach(io: File.open("#{Rails.root}/db/fixtures/sample-user3.jpg"), filename:"sample-user3.jpg")
+
 # Post.create!(
 #  [
 #    {title: '窓拭きに役立つあるもの', image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post1.jpg"), filename:"sample-post1.jpg"), body: '窓拭きの際には丸めた新聞紙で水拭き・乾拭きを行うと、洗剤を使わなくても綺麗になります。', user_id: users[0].id, genre_id: genres[0].id},
@@ -37,3 +43,11 @@ olivia.profile_image.attach(io: File.open("#{Rails.root}/db/fixtures/sample-user
 #    {title: 'ハンバーグ作りの時に少しだけ役立つかも', image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post3.jpg"), filename:"sample-post3.jpg"), body: 'ハンバーグをこねた後の手の汚れは砂糖で落とすことができます。砂糖小さじ1杯を手に取り10秒位こすり合わせて流し、それからハンドソープで洗うとスッキリきれいになります。', user_id: users[2].id, genre_id: genres[3].id}
 #  ]
 #)
+post1 = Post.create!(title: '窓拭きに役立つあるもの', body: '窓拭きの際には丸めた新聞紙で水拭き・乾拭きを行うと、洗剤を使わなくても綺麗になります。', user_id: olivia.id, genre_id: genres[0].id)
+post1.image.attach(io: File.open("#{Rails.root}/db/fixtures/sample-post1.jpg"), filename:"sample-post1.jpg")
+
+post2 = Post.create!(title: '食費を抑える方法', body: '食費を抑えるたったひとつの簡単な方法、それは毎月の食費を予め封筒に入れておくことです！そうすることで不思議とその金額内で収めようとする心理的な効果が働くので、ぜひ一度試してみてください。', user_id: james.id, genre_id: genres[0].id)
+post2.image.attach(io: File.open("#{Rails.root}/db/fixtures/sample-post2.jpg"), filename:"sample-post2.jpg")
+
+post3 = Post.create!(title: 'ハンバーグ作りの時に少しだけ役立つかも', body: 'ハンバーグをこねた後の手の汚れは砂糖で落とすことができます。砂糖小さじ1杯を手に取り10秒位こすり合わせて流し、それからハンドソープで洗うとスッキリきれいになります。', user_id: lucas.id, genre_id: genres[0].id)
+post3.image.attach(io: File.open("#{Rails.root}/db/fixtures/sample-post3.jpg"), filename:"sample-post3.jpg")
